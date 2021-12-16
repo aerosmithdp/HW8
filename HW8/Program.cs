3290b99a08b50fb606002da0a39dc5b1b896f645
aerosmith_dp <aerosmith.dp@gmail.com>
2021-12-16 22:28:36 +0200

    init

---
+++ HW8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HW8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double x;
+            //double baseLog;
+            //Console.WriteLine("Вычисление логарифма");
+            //Console.Write("Введите х: ");
+            //x = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите основание логарифма: ");
+            //baseLog = Convert.ToDouble(Console.ReadLine());
+            //double result = Math.Log(x, baseLog);
+            //Console.WriteLine($"Ответ: {result}");
+
+
+
+
+
+            //double a;
+            //Console.WriteLine("Вычисление интеграла");
+            //Console.Write("Введите а: ");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(a +"x + C");
+
+
+
+
+
+
+            //Console.WriteLine("Определение расположения точки на координатной плоскости");
+            //Console.Write("Введите x: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите y: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //if (x > 0 && y > 0)
+            //    Console.WriteLine("I четверть");
+            //else if (x < 0 && y > 0)
+            //    Console.WriteLine("II четверть");
+            //else if (x < 0 && y < 0)
+            //    Console.WriteLine("III четверть");
+            //else if (x > 0 && y < 0)
+            //    Console.WriteLine("IV четверть");
+            //else if (x > 0 && y == 0)
+            //    Console.WriteLine("Точка на оси X");
+            //else if (x == 0 && y > 0)
+            //    Console.WriteLine("Точка на оси Y");
+            //else if (x == 0 && y == 0)
+            //    Console.WriteLine("Точка начала координат");
+        }
+    }
+}
